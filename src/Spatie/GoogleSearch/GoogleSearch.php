9e732875a5c993a904d298d64548edc09535a63c
freek <freek@spatie.be>
2014-12-17 11:46:57 +0100

    add Pagemap properties to result

--- src/Spatie/GoogleSearch/GoogleSearch.php
+++ src/Spatie/GoogleSearch/GoogleSearch.php
@@ -1,5 +1,6 @@
 <?php namespace Spatie\GoogleSearch;
 
+use Exception;
 use Spatie\GoogleSearch\Interfaces\GoogleSearchInterface;
 use GuzzleHttp\Client;
 
@@ -17,7 +18,8 @@ class GoogleSearch implements GoogleSearchInterface {
      *
      * @param string $query
      * @return array An associative array of the parsed comment, whose keys are `name`,
-     *         `url` and `snippet`
+     *         `url` and `snippet` and some others
+     * @throws Exception
      */
     public function getResults($query) {
 
@@ -44,7 +46,7 @@ class GoogleSearch implements GoogleSearchInterface {
 
 
         if ($result->getStatusCode() != 200) {
-            throw new \Exception('Resultcode was not ok: ' . $result->getStatusCode());
+            throw new Exception('Resultcode was not ok: ' . $result->getStatusCode());
         }
 
         $xml = simplexml_load_string($result->getBody());
@@ -55,10 +57,39 @@ class GoogleSearch implements GoogleSearchInterface {
                 $searchResults[$i]['name'] = (string)$item->T;
                 $searchResults[$i]['url'] = (string)$item->U;
                 $searchResults[$i]['snippet'] = (string)$item->S;
+                $searchResults[$i]['image'] = $this->getPageMapProperty($item, 'cse_image', 'src');
+
+                $searchResults[$i]['product']['name'] = $this->getPageMapProperty($item, 'product', 'name');
+                $searchResults[$i]['product']['brand'] = $this->getPageMapProperty($item, 'product', 'brand');
+                $searchResults[$i]['product']['price'] = $this->getPageMapProperty($item, 'product', 'price');
+                $searchResults[$i]['product']['image'] = $this->getPageMapProperty($item, 'product', 'image');
+                $searchResults[$i]['offer']['price'] = $this->getPageMapProperty($item, 'offer', 'price');
+                $searchResults[$i]['offer']['pricecurrency'] = $this->getPageMapProperty($item, 'offer', 'pricecurrency');
+
                 $i++;
             }
         }
 
         return $searchResults;
+    }
+
+    /**
+     * Get a the value Pagemap property with the given name of the given type of the given item
+     *
+     * @param $item
+     * @param $type
+     * @param $attribute
+     * @return string
+     */
+    public function getPageMapProperty($item, $type, $attribute)
+    {
+        $propertyArray = $item->PageMap->xpath('DataObject[@type="' . $type . '"]/Attribute[@name="' . $attribute . '"]/@value');
+
+        if (! count($propertyArray))
+        {
+            return '';
+        }
+
+        return (string)$propertyArray[0];
     }
 }
